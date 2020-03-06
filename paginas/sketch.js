58fc8e6774fb052925da0f4bc52771598089243e
Carlos Alexis Aguirre Castillo <carlospoqw@gmail.com>
2020-03-06 00:03:59 -0300

    dsfd
    sdfsd

--- paginas/sketch.js
+++ paginas/sketch.js
@@ -1,7 +1,24 @@
+var song;
+var slider;
+
+function preload() {
+    song = loadSound("song.mp3");
+    createCanvas(1, 700);
+}
+
 function setup() {
-  // put setup code here
+    // put setup code here
+
+    song.play();
+    slider = createSlider(0, 1, 0.0, 0.01);
+
 }
 
 function draw() {
-  // put drawing code here
+    // put drawing code here
+    background(153);
+    line(0, 0, width, height);
+    song.setVolume(slider.value());
+    $("#defaultCanvas0").appendTo("#one");
+
 }
