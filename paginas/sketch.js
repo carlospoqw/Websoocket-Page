d2dde9bdb2271900c3ea081347f3f33c876dfdce
Carlos Alexis Aguirre Castillo <carlospoqw@gmail.com>
2020-03-06 17:19:59 -0300

    Faltan los gráficos
    Tiene sonido, boton de pausa, volumen y velocidad

--- paginas/sketch.js
+++ paginas/sketch.js
@@ -1,24 +1,50 @@
 var song;
-var slider;
+var sliderVolume;
+var sliderSpeed;
+var canvas;
+var canvasWidth = 500;
+var canvasHeight = 500;
+var canvasX = 20;
+var canvasY = 50;
+var buttonPlayStop;
+var divGame = document.getElementById("one");
 
 function preload() {
     song = loadSound("song.mp3");
-    createCanvas(1, 700);
 }
 
 function setup() {
     // put setup code here
-
     song.play();
-    slider = createSlider(0, 1, 0.0, 0.01);
-
+    canvas = createCanvas(canvasWidth, canvasHeight);
+    background(153);
+    canvas.position(canvasX, canvasY);
+    buttonPlayStop = createButton("PAUSE");
+    sliderVolume = createSlider(0, 1, 0.01, 0.001);
+    sliderSpeed = createSlider(1, 2, 1, 0.01);
+    buttonPlayStop.position(canvasX, canvasY + canvasHeight);
+    sliderVolume.position(canvasX, 20);
+    sliderSpeed.position(canvasX, 5);
+    buttonPlayStop.style('width', canvasWidth + 'px');
+    sliderVolume.style('width', canvasWidth + 'px');
+    sliderSpeed.style('width', canvasWidth + 'px');
+    buttonPlayStop.mousePressed(playStop);
 }
 
 function draw() {
     // put drawing code here
-    background(153);
-    line(0, 0, width, height);
-    song.setVolume(slider.value());
-    $("#defaultCanvas0").appendTo("#one");
+    song.setVolume(sliderVolume.value());
+    song.rate(sliderSpeed.value());
 
+}
+
+
+function playStop() {
+    if (song.isPlaying()) {
+        song.pause();
+        buttonPlayStop.html('PLAY');
+    } else {
+        song.play();
+        buttonPlayStop.html('PAUSE');
+    }
 }
