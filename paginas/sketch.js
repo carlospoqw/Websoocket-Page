5b5bbd51896b5f1767e22549395404bd9d257e6c
Carlos Alexis Aguirre Castillo <carlospoqw@gmail.com>
2020-03-10 17:01:32 -0300

    Dibujo Listo
    sin errores

--- paginas/sketch.js
+++ paginas/sketch.js
@@ -8,6 +8,9 @@ var canvasX = 20;
 var canvasY = 50;
 var buttonPlayStop;
 var divGame = document.getElementById("one");
+var songAmplitude;
+var volHistory = [];
+volHistory = Array(360).fill(0);
 
 function preload() {
     song = loadSound("song.mp3");
@@ -16,6 +19,7 @@ function preload() {
 function setup() {
     // put setup code here
     song.play();
+
     canvas = createCanvas(canvasWidth, canvasHeight);
     background(153);
     canvas.position(canvasX, canvasY);
@@ -29,11 +33,35 @@ function setup() {
     sliderVolume.style('width', canvasWidth + 'px');
     sliderSpeed.style('width', canvasWidth + 'px');
     buttonPlayStop.mousePressed(playStop);
+    songAmplitude = new p5.Amplitude();
+
+    angleMode(DEGREES);
+
 }
 
 function draw() {
     // put drawing code here
     song.setVolume(sliderVolume.value());
     song.rate(sliderSpeed.value());
+    background(0);
+    push();
+    var songVolume = songAmplitude.getLevel();
+    volHistory.push(songVolume);
+    stroke(255);
+    noFill();
+    translate(canvasWidth / 2, canvasHeight / 2);
+    beginShape();
+
+    for (var i = 0; i < 360; i++) {
+        var r = map(volHistory[i], 0, 1, 10, canvasHeight);
+        let x = r * cos(i);
+        let y = r * sin(i);
+        vertex(x, y);
+    }
+    endShape();
+
+    if (volHistory.length > 360) {
+        volHistory.splice(0, 1);
+    }
 
 }
